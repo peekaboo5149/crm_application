17e55df2fd02ec88cb080ba573c0755d259f61cc
peekaboo5149 <ktanmay5149@gmail.com>
2022-02-10 03:04:36 +0530

    Bug fixed

--- src/main/java/nerds/jaguar/MovieResources.java
+++ src/main/java/nerds/jaguar/MovieResources.java
@@ -4,10 +4,9 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 
 @Path("admin")
@@ -28,10 +27,9 @@ public class MovieResources {
     }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     @Path("count")
-    public Long count() {
-        return 0L;
+    public Uni<Integer> count() {
+        return Admin.count(client);
     }
 
     private void initDb() {
